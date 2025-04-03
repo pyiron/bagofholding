8d6e88669b15ac44bab79279032e3991e5222cae
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-03 14:10:45 -0700

    Remove unavailable imports
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- bagofholding/__init__.py
+++ bagofholding/__init__.py
@@ -1,3 +1,1 @@
 from bagofholding.h5.bag import ClassH5Bag as ClassH5Bag
-from bagofholding.instances.h5 import InstanceH5Bag as InstanceH5Bag
-from bagofholding.instances.pickle import PickleBag as PickleBag
