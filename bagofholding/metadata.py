f91c751d8d260e4e3cea17bc89176a4dfbaa6647
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-04 10:34:58 -0700

    Make version scraping customizable
    And make methods only used internally private
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- bagofholding/metadata.py
+++ bagofholding/metadata.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from collections.abc import ItemsView
+from collections.abc import Callable, ItemsView
 from dataclasses import asdict, dataclass
 from importlib import import_module
 from sys import version_info
@@ -18,33 +18,57 @@ class Metadata:
         return asdict(self).items()
 
 
-def get_metadata(obj: Any) -> Metadata | None:
-    module = get_module(obj)
+def get_metadata(
+    obj: Any,
+    version_scraping: dict[str, Callable[[str], str | None]] | None = None,
+) -> Metadata | None:
+    """
+
+    Args:
+        obj (Any): The object who's module to extract metadata from.
+        version_scraping (dict[str, Callable[[str], str]] | None): An optional
+            dictionary mapping module names to a callable that takes this name and
+            returns a version (or None). The default callable imports the module
+            string and looks for a `__version__` attribute.
+
+    Returns:
+        (Metadata|None): The metadata extracted from the object, or `None` if the
+            object is builtin.
+    """
+    module = _get_module(obj)
     if module == "builtins":
         return None
     else:
         return Metadata(
             qualname=obj.__class__.__qualname__,
             module=module,
-            version=get_version(module),
+            version=_get_version(
+                module, {} if version_scraping is None else version_scraping
+            ),
             meta=str(obj.__metadata__) if hasattr(obj, "__metadata__") else None,
         )
 
 
-def get_module(obj: Any) -> str:
+def _get_module(obj: Any) -> str:
     return obj.__module__ if isinstance(obj, type) else type(obj).__module__
 
 
-def get_version(module_name: str) -> str | None:
+def _get_version(
+    module_name: str, version_scraping: dict[str, Callable[[str], str | None]]
+) -> str | None:
     if module_name == "builtins":
         return f"{version_info.major}.{version_info.minor}.{version_info.micro}"
 
-    module = import_module(module_name.split(".")[0])
-    for version in ["__version__", "version", "_version"]:
-        if hasattr(module, version):
-            return str(getattr(module, version))
+    scraper = version_scraping.get(module_name, _scrape_version_attribute)
+    return scraper(module_name)
 
-    return None
+
+def _scrape_version_attribute(module_name: str) -> str | None:
+    module = import_module(module_name.split(".")[0])
+    try:
+        return str(module.__version__)
+    except AttributeError:
+        return None
 
 
 @dataclass(frozen=True)
---
+++ tests/unit/test_metadata.py
@@ -0,0 +1,54 @@
+import sys
+import unittest
+
+import bidict
+
+from bagofholding.metadata import _get_module, _get_version
+
+
+def some_version_scraper(module_name: str) -> str | None:
+    return "some.version"
+
+
+class TestMetadata(unittest.TestCase):
+    def test_get_module(self):
+        self.assertEqual("builtins", _get_module(int), msg="Should work with types")
+        self.assertEqual("builtins", _get_module(5), msg="Should work with instances")
+
+    def test_version_scraping(self):
+
+        py_version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+        self.assertEqual(
+            py_version,
+            _get_version(_get_module(int), {}),
+            msg="builtins should return the python version",
+        )
+
+        self.assertEqual(
+            py_version,
+            _get_version(_get_module(5), {"builtins": some_version_scraper}),
+            msg="builtins should _always_ just return the python version",
+        )
+
+        self.assertIsNone(
+            _get_version("static", {}),
+            msg="Modules without a version are expected to return None",
+        )
+
+        self.assertEqual(
+            bidict.__version__,
+            _get_version("bidict", {}),
+            msg="This is the fundamental behaviour of the default",
+        )
+
+        self.assertEqual(
+            some_version_scraper("foo"),
+            _get_version("bidict", {"bidict": some_version_scraper}),
+            msg="Users can override how versions are scraped for a particular module",
+        )
+
+        self.assertEqual(
+            bidict.__version__,
+            _get_version("bidict", {"not_bidict": some_version_scraper}),
+            msg="Modules shouldn't care about other modules' overrides",
+        )
