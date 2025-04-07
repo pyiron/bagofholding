b181fedf6b3aaeb6bb729aae423c480ce8a60e38
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-07 10:49:45 -0700

    Test version checking the bag
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- tests/unit/test_h5bag.py
+++ tests/unit/test_h5bag.py
@@ -6,8 +6,11 @@ import contextlib
 import os
 import unittest
 
+import numpy as np
+
 from bagofholding.bag import BagMismatchError
 from bagofholding.h5.bag import H5Bag, H5Info
+from bagofholding.metadata import EnvironmentMismatch
 
 
 class BagVariant(H5Bag):
@@ -16,9 +19,13 @@ class BagVariant(H5Bag):
         return H5Info(
             qualname=cls.__qualname__,
             module=cls.__module__,
-            version="Something different",
+            version=always_42(),
             libver_str=cls.libver_str,
         )
+
+
+def always_42(module_name: str = "not even used") -> str:
+    return "42"
 
 
 class TestBag(unittest.TestCase):
@@ -26,17 +33,36 @@ class TestBag(unittest.TestCase):
     def setUpClass(cls):
         cls.save_name = "savefile.h5"
 
-    def setUp(self):
-        H5Bag.save(42, self.save_name)
-
     def tearDown(self):
         with contextlib.suppress(FileNotFoundError):
             os.remove(self.save_name)
 
     def test_bag_info_check(self):
+        H5Bag.save(42, self.save_name)
         H5Bag(self.save_name)
 
         with self.assertRaises(
             BagMismatchError, msg="We expect to fail hard when bag info mismatches"
         ):
             BagVariant(self.save_name)
+
+    def test_version_checking(self):
+        obj = np.polynomial.Polynomial([1, 2, 3])
+
+        H5Bag.save(obj, self.save_name)
+        bag = H5Bag(self.save_name)
+        self.assertEqual(
+            np.__version__,
+            bag["object"].version,
+            msg="Object version metadata should be automatically scraped",
+        )
+        with self.assertRaises(
+            EnvironmentMismatch, msg="Fail hard when env mismatches"
+        ):
+            bag.load(version_scraping={"numpy": always_42})
+
+        self.assertEqual(
+            obj,
+            bag.load(version_scraping={"not_numpy": always_42}),
+            msg="Ignore scrapers for irrelevant modules",
+        )
