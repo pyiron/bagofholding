78adc62397cff130fc25a0d8bfff803f014db101
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-09 13:30:01 -0700

    Add proper readme
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- tests/integration/test_readme.py
+++ tests/integration/test_readme.py
@@ -1,9 +1,25 @@
+import contextlib
 import doctest
+import os
 import unittest
 
 
+def remove_save_file(_):
+    for filename in ["file.h5", "something.h5", "custom.h5"]:
+        with contextlib.suppress(FileNotFoundError):
+            os.remove(filename)
+
+
 def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocFileSuite("../../docs/README.md"))
+    tests.addTests(
+        doctest.DocFileSuite(
+            "../../docs/README.md",
+            optionflags=doctest.ELLIPSIS,
+            tearDown=remove_save_file,
+            globs={"__name__": "__main__"},
+        )
+    )
+
     return tests
 
 
