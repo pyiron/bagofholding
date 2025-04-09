8ecf4e0584ca4ea78c75b311b310611d0f51b58a
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-09 13:06:34 -0700

    Add exceptions to the API
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- bagofholding/__init__.py
+++ bagofholding/__init__.py
@@ -2,4 +2,14 @@ from . import _version
 
 __version__ = _version.get_versions()["version"]
 
+from bagofholding.bag import BagMismatchError as BagMismatchError
 from bagofholding.h5.bag import H5Bag as H5Bag
+from bagofholding.metadata import (
+    EnvironmentMismatchError as EnvironmentMismatchError,
+)
+from bagofholding.metadata import (
+    ModuleForbiddenError as ModuleForbiddenError,
+)
+from bagofholding.metadata import (
+    NoVersionError as NoVersionError,
+)
