d7242e2b76e883f48384ac5dc168f4cf592ed561
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-10 13:26:35 -0700

    Rename notebook
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- tests/integration/test_notebook.py
+++ tests/integration/test_notebook.py
@@ -11,6 +11,6 @@ class TestNotebook(unittest.TestCase):
         directly.
         """
         print(__file__)
-        result = subprocess.run(["pytest", "--nbval", os.path.join(os.path.dirname(__file__), "../../notebooks/example.ipynb")])
+        result = subprocess.run(["pytest", "--nbval", os.path.join(os.path.dirname(__file__), "../../notebooks/tutorial.ipynb")])
         if result.returncode != 0:
             self.fail(f"nbval failed with exit code {result.returncode}")
