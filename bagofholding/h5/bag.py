93800a293406dfe3e1d76e2130ad09ce16512bae
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-16 06:38:17 -0700

    Extract shared behaviour
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- bagofholding/h5/bag.py
+++ bagofholding/h5/bag.py
@@ -201,8 +201,11 @@ class H5Bag(Bag[H5Info]):
     def maybe_decode(attr: str | bytes) -> str:
         return attr if isinstance(attr, str) else attr.decode("utf-8")
 
-    def pack_empty(self, path: str) -> None:
-        self.file.create_dataset(path, data=h5py.Empty(dtype="f"))
+    def _pack_raw(self, obj: bytearray | bool | int | float, path: str) -> None:
+        self.file.create_dataset(path, data=obj)
+
+    def _unpack_raw(self, path: str) -> Any:
+        return self.file[path][()]
 
     def pack_string(self, obj: str, path: str) -> None:
         self.file.create_dataset(
@@ -210,44 +213,47 @@ class H5Bag(Bag[H5Info]):
         )
 
     def unpack_string(self, path: str) -> str:
-        return cast(str, self.file[path][()].decode("utf-8"))
+        return cast(str, self._unpack_raw(path).decode("utf-8"))
 
-    def pack_bytes(self, obj: bytes, path: str) -> None:
-        self.file.create_dataset(path, data=np.void(obj))
-
-    def unpack_bytes(self, path: str) -> bytes:
-        return bytes(self.file[path][()])
-
-    def pack_bool(self, obj: bool, path: str) -> None:
-        self.file.create_dataset(path, data=obj)
-
-    def unpack_bool(self, path: str) -> bool:
-        return bool(self.file[path][()])
-
-    def pack_long(self, obj: int, path: str) -> None:
-        self.file.create_dataset(path, data=obj)
-
-    def unpack_long(self, path: str) -> int:
-        return int(self.file[path][()])
-
-    def pack_float(self, obj: float, path: str) -> None:
-        self.file.create_dataset(path, data=obj)
-
-    def unpack_float(self, path: str) -> float:
-        return float(self.file[path][()])
+    def pack_empty(self, path: str) -> None:
+        self.file.create_dataset(path, data=h5py.Empty(dtype="f"))
 
     def pack_complex(self, obj: complex, path: str) -> None:
         self.file.create_dataset(path, data=np.array([obj.real, obj.imag]))
 
     def unpack_complex(self, path: str) -> complex:
-        entry = self.file[path][()]
-        return complex(entry[0], entry[1])
+        data = self._unpack_raw(path)
+        return complex(data[0], data[1])
+
+    def pack_bytes(self, obj: bytes, path: str) -> None:
+        self.file.create_dataset(path, data=np.void(obj))
+
+    def unpack_bytes(self, path: str) -> bytes:
+        return bytes(self._unpack_raw(path))
+
+    def pack_bool(self, obj: bool, path: str) -> None:
+        return self._pack_raw(obj, path)
+
+    def unpack_bool(self, path: str) -> bool:
+        return bool(self._unpack_raw(path))
+
+    def pack_long(self, obj: int, path: str) -> None:
+        return self._pack_raw(obj, path)
+
+    def unpack_long(self, path: str) -> int:
+        return int(self._unpack_raw(path))
+
+    def pack_float(self, obj: float, path: str) -> None:
+        return self._pack_raw(obj, path)
+
+    def unpack_float(self, path: str) -> float:
+        return float(self._unpack_raw(path))
 
     def pack_bytearray(self, obj: bytearray, path: str) -> None:
-        self.file.create_dataset(path, data=obj)
+        return self._pack_raw(obj, path)
 
     def unpack_bytearray(self, path: str) -> bytearray:
-        return bytearray(self.file[path][()])
+        return bytearray(self._unpack_raw(path))
 
     def create_group(self, path: str) -> None:
         self.file.create_group(path)
