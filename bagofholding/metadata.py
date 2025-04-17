4e6fc5b217aea170b3a7b11332f72d74fce70bae
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-17 06:47:59 -0700

    Make Metadata frozen
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- bagofholding/metadata.py
+++ bagofholding/metadata.py
@@ -10,7 +10,7 @@ from typing import Any, Literal, TypeAlias
 from bagofholding.exceptions import EnvironmentMismatchError
 
 
-@dataclass
+@dataclass(frozen=True)
 class Metadata:
     content_type: str
     qualname: str | None = None
