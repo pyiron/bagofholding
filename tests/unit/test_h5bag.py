7d75f7b805ce39d2dca50f48d3a2b74f888d04d4
liamhuber <liamhuber@greyhavensolutions.com>
2025-04-28 13:08:07 -0700

    Relocate and merge implementation tests
    Mypy was having trouble resolving the inheritance from static
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- tests/unit/test_h5bag.py
+++
@@ -1,9 +0,0 @@
-import abstract_bag_test
-
-from bagofholding.h5.bag import H5Bag
-
-
-class TestRestructuredH5Bag(abstract_bag_test.AbstractBagTest):
-    @classmethod
-    def bag_class(cls) -> type[H5Bag]:
-        return H5Bag
--- tests/unit/test_trieh5bag.py
+++
@@ -1,9 +0,0 @@
-import abstract_bag_test
-
-from bagofholding.h5.triebag import TrieH5Bag
-
-
-class TestRestructuredH5Bag(abstract_bag_test.AbstractBagTest):
-    @classmethod
-    def bag_class(cls) -> type[TrieH5Bag]:
-        return TrieH5Bag
