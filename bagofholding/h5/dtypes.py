5df92c7cf9863ff7b48996a366ec9a3961542132
liamhuber <liamhuber@greyhavensolutions.com>
2025-06-23 11:34:24 -0700

    Use | instead of typing.Union for ruff
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- bagofholding/h5/dtypes.py
+++ bagofholding/h5/dtypes.py
@@ -4,7 +4,7 @@ h5py supports efficient treatment of a subset of possible array.
 Here, we whitelist these types.
 """
 
-from typing import TypeAlias, Union
+from typing import TypeAlias
 
 import numpy as np
 
@@ -28,26 +28,26 @@ H5PY_DTYPE_WHITELIST = (
 )
 
 
-IntTypesAlias: TypeAlias = Union[
-    np.dtype[np.int8],
-    np.dtype[np.int16],
-    np.dtype[np.int32],
-    np.dtype[np.int64],
-    np.dtype[np.uint8],
-    np.dtype[np.uint16],
-    np.dtype[np.uint32],
-    np.dtype[np.uint64],
-]
+IntTypesAlias: TypeAlias = (
+    np.dtype[np.int8]
+    | np.dtype[np.int16]
+    | np.dtype[np.int32]
+    | np.dtype[np.int64]
+    | np.dtype[np.uint8]
+    | np.dtype[np.uint16]
+    | np.dtype[np.uint32]
+    | np.dtype[np.uint64]
+)
 
 
-H5DtypeAlias: TypeAlias = Union[
-    IntTypesAlias,
-    np.dtype[np.float16],
-    np.dtype[np.float32],
-    np.dtype[np.float64],
-    np.dtype[np.complex64],
-    np.dtype[np.complex128],
-    np.dtype[np.bool_],
-    np.dtype[np.bytes_],
-    np.dtype[np.str_],
-]
+H5DtypeAlias: TypeAlias = (
+    IntTypesAlias
+    | np.dtype[np.float16]
+    | np.dtype[np.float32]
+    | np.dtype[np.float64]
+    | np.dtype[np.complex64]
+    | np.dtype[np.complex128]
+    | np.dtype[np.bool_]
+    | np.dtype[np.bytes_]
+    | np.dtype[np.str_]
+)
