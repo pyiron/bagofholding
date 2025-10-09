8836f34de840a30dd4394607132ebdae06174719
liamhuber <liamhuber@greyhavensolutions.com>
2025-10-09 09:50:27 -0700

    Update expected error message
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- tests/unit/test_bag.py
+++ tests/unit/test_bag.py
@@ -1,6 +1,7 @@
 import contextlib
 import io
 import pickle
+import string
 import sys
 import unittest
 
@@ -16,11 +17,21 @@ class TestBag(unittest.TestCase):
             Bag.pickle_check(unpickleable)
 
         msg = Bag.pickle_check(unpickleable, raise_exceptions=False)
-        verb = "get" if sys.version_info >= (3, 12) else "pickle"
-        self.assertEqual(
-            f"Can't {verb} local object 'TestBag.test_pickle_helper.<locals>.<lambda>'",
-            msg,
+        msg_reference = string.Template("Can't $verb local object $obj")
+
+        verb = (
+            "get"
+            if sys.version_info >= (3, 12) and sys.version_info < (3, 14)
+            else "pickle"
         )
+        obj_reference = "TestBag.test_pickle_helper.<locals>.<lambda>"
+        obj = (
+            f"'{obj_reference}'"
+            if sys.version_info < (3, 14)
+            else f"<function {obj_reference} at {id(unpickleable):#x}"
+        )
+
+        self.assertEqual(msg_reference.substitute(verb=verb, obj=obj), msg)
 
         buf = io.StringIO()
         with contextlib.redirect_stdout(buf):
