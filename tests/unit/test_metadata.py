6bfb751e940e34f105ed1a8fb14e500c7baa969a
liamhuber <liamhuber@greyhavensolutions.com>
2026-09-11 14:59:40 -0700

    Use snippets instead of bidict
    To test versioning metadata, since bidict yanked `bidict.__version__` (cf. [changelog](https://bidict.readthedocs.io/en/v0.24.1/changelog.html#id2))
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- tests/unit/test_metadata.py
+++ tests/unit/test_metadata.py
@@ -1,7 +1,7 @@
 import sys
 import unittest
 
-import bidict
+import pyiron_snippets
 import numpy as np
 
 from bagofholding import EnvironmentMismatchError
@@ -74,20 +74,22 @@ class TestMetadata(unittest.TestCase):
         )
 
         self.assertEqual(
-            bidict.__version__,
-            get_version("bidict", {}),
+            pyiron_snippets.__version__,
+            get_version("pyiron_snippets", {}),
             msg="This is the fundamental behaviour of the default",
         )
 
         self.assertEqual(
             some_version_scraper("foo"),
-            get_version("bidict", {"bidict": some_version_scraper}),
+            get_version("pyiron_snippets", {"pyiron_snippets": some_version_scraper}),
             msg="Users can override how versions are scraped for a particular module",
         )
 
         self.assertEqual(
-            bidict.__version__,
-            get_version("bidict", {"not_bidict": some_version_scraper}),
+            pyiron_snippets.__version__,
+            get_version(
+                "pyiron_snippets", {"not_pyiron_snippets": some_version_scraper}
+            ),
             msg="Modules shouldn't care about other modules' overrides",
         )
 
